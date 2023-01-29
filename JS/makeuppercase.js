2a6a42fea5ca1cd6611d2b15150758a699d0cbbc
dinako81 <dina.koliata@gmail.com>
2023-01-29 23:47:27 +0200

    Kata training

---
+++ JS/makeuppercase.js
@@ -0,0 +1,13 @@
+// MakeUpperCase
+// Write a function which converts the input string to uppercase.
+
+
+function makeUpperCase(str) {
+    return str.toLocaleUpperCase();
+  }
+
+// more optimal
+
+function makeUpperCase(str) {return str.toUpperCase();}
+
+
